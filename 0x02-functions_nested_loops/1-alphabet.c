eee93b137b5cd9858df73d88c29a69588e74adaf
leFrasch <mburu.johnn3@gmail.com>
2022-03-16 07:37:20 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
