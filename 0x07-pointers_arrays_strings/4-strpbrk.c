44c6c6c1e7b1681f9ebb005f1f9780e6b4064871
leFrasch <mburu.johnn3@gmail.com>
2022-03-29 23:23:44 -0700

    Searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,18 +8,18 @@
 **/
 char *_strpbrk(char *s, char *accept)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; accept[j] != '\0'; j++)
-                {
-                        if (accept[j] == s[i])
-                        {
-                                s = s + i;
-                                return (s);
-                        }
-                }
-        }
-        return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				s = s + i;
+				return (s);
+			}
+		}
+	}
+	return (0);
 }
