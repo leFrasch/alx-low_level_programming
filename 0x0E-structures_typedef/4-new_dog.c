8f9444e78193d4687653f530f9c8403570cf28fb
leFrasch <mburu.johnn3@gmail.com>
2022-04-11 04:39:29 -0700

    How many doors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,80 +1,86 @@
 #include <stdio.h>
-#include "dog.h"
 #include <stdlib.h>
-/**
- * _strlen - returns the length of a string
- *
- * @s: string to check length
- * Return: returns the length of string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	i++;
-	return (i);
-}
+#include "dog.h"
 
 /**
- * _strcpy - copies an entire string to another
+ * _strcpy - copies the string pointed to by src,
+ * @dest: destnation poiter to take value
+ * @src: array poited that gets copied
  *
- * @src: source string
- * @dest: destination buffer
- * Return: returns pointer to copied string
+ * Description: copies string pointed to by src,
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i, length = _strlen(src);
-	char *retString;
+	int i;
 
-	for (i = 0 ; i <= length ; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	retString = dest;
-	return (retString);
+	dest[i] = '\0';
+	return (dest);
 }
 
 /**
- * new_dog - creates a new dog structure
- *
+ * _strlen_recursion - function that returns the length of a string
+ * @s: pointer to a string
+ * Description: returns the length of a string
+ * Return: 0 for no string, i + 1 (length of string) for success
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		i = (_strlen_recursion(s + 1));
+		return (i + 1);
+	}
+}
+/**
+ * new_dog - function that creates a new dog
  * @name: name of dog
  * @age: age of dog
- * @owner: dog's owner
- * Return: returns pointer to the new dog structure
+ * @owner: name of owner
+ *
+ * Description: a function that creates a new dog
+ * Return: new_dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog_copy;
+	dog_t *new_dog;
 
-	dog_copy = malloc(sizeof(dog_t));
-	if (dog_copy)
+	if (name == NULL || owner == NULL)
 	{
-		dog_copy->name = malloc(sizeof(char) * _strlen(name));
-		if (dog_copy->name)
-		{
-			dog_copy->owner = malloc(sizeof(char) * _strlen(owner));
-			if (dog_copy->owner)
-			{
-				_strcpy(dog_copy->name, name);
-				dog_copy->age = age;
-				_strcpy(dog_copy->owner, owner);
-				return (dog_copy);
-			}
-			else
-			{
-				free(dog_copy->name);
-				free(dog_copy);
-			}
-		}
-		else
-		{
-			free(dog_copy);
-		}
+		return (NULL);
 	}
-	return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = malloc(sizeof(char) * _strlen_recursion(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * _strlen_recursion(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+	return (new_dog);
 }
